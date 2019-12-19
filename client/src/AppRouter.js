9e40b51f8478fb282d02b5ad2fd7260565bc2431
Alejandro Fabian Campos <camposalejandrofabian@gmail.com>
2019-12-18 21:55:39 -0300

    merging

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,12 +1,41 @@
+<<<<<<< Updated upstream
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, browserHistory, Switch } from 'react-router-dom';
+=======
+import React, { Suspense, Component } from 'react'
+import { BrowserRouter as Router, Route, browserHistory, Switch } from 'react-router-dom';
+
+>>>>>>> Stashed changes
 import { CircularProgress } from '@material-ui/core';
+
+import io from 'socket.io-client';
 
 const Landing = React.lazy(() => import('./pages/landing'));
 
-export default function AppRouter() {
-    return (
-        <Router>
+
+export default class AppRouter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+           
+        }
+
+    }
+
+    componentWillMount () {
+        this.initializeSocket()
+    }
+
+    initializeSocket = () => {
+        console.log('Socket url ->', process.env.REACT_APP_SOCKET_URL)
+        this.socket = io(process.env.REACT_APP_SOCKET_URL)
+        console.log('Created socket ->', this.socket)
+    }
+    
+    render() {
+        return (
+            <Router>
             <Suspense fallback={ <CircularProgress /> }>
                 <Switch>
                     <Route path="/" >
@@ -15,5 +44,6 @@ export default function AppRouter() {
                 </Switch>
             </Suspense>
         </Router>
-    )
+        )
+    }
 }
